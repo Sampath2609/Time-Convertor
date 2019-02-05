d0434663a8d53a699c0ccdf7299858a2d4c468ed
Vagicharla,Venkat Raghava Sampath <s533978@ch140016m.local>
2019-02-05 12:50:11 -0600

    Initial Commit

---
+++ Time Convertor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Convertor
+//
+//  Created by Vagicharla,Venkat Raghava Sampath on 2/5/19.
+//  Copyright © 2019 Vagicharla,Venkat Raghava Sampath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
